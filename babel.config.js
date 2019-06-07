3785b83343420eaeb373d2a4e663505bef8dc44c
SECT <info@sectwebstudio.com>
2019-06-07 15:19:17 +0900

    Migrate to Babel@7.4 / core-js@3 & Update dependencies

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,8 @@ module.exports = api => {
   api.cache(true);
   const presets = [
     ["@babel/preset-env", {
-      "useBuiltIns": "usage"
+      useBuiltIns: "usage",
+      corejs: 3,
     }]
   ];
 
