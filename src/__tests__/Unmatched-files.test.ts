0babc33186ccd27a3e6596b0bc41b0e57e6844d4
SECT <info@sectwebstudio.com>
2021-06-02 00:20:41 +0900

    Add a test to check unmatched files

---
+++ src/__tests__/Unmatched-files.test.ts
@@ -0,0 +1,13 @@
+import * as path from 'path';
+import { WebpackSweetEntry } from '../index';
+
+const tsPath = path.join(__dirname, 'files/unmatched');
+const abPath = __dirname.replace('/webpack-sweet-entry/src/__tests__', '');
+const wse = WebpackSweetEntry(path.resolve(tsPath, '**/*.js*'), 'js', 'js');
+const obj = Object.assign({}, ...Object.keys(wse).map(k => ({ [k]: wse[k].replace(abPath, '') })));
+
+describe('Unmatched Files Test', () => {
+  it('toEqual()', () => {
+    expect(obj).toEqual({});
+  });
+});
