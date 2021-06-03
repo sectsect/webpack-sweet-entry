ff19074c59cc070314becf2dffb081c5cf8605fb
SECT <info@sectwebstudio.com>
2021-06-04 02:36:47 +0900

    chore: Update dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,79 +1,76 @@
 module.exports = {
-  "env": {
-    "es6": true,
-    "browser": true,
-    "node": true,
-    "jquery": true
+  env: {
+    es6: true,
+    browser: true,
+    node: true,
+    jquery: true,
   },
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
     // "ecmaVersion": 2020,
-    "sourceType": "module",
-    "project": "./tsconfig.json"
+    sourceType: 'module',
+    project: './tsconfig.json',
   },
-  "extends": [
-    "airbnb-typescript",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended"
-  ],
-  "plugins": [
-    "@typescript-eslint"
-  ],
-  "ignorePatterns": ["__tests__"],
-  "rules": {
-    "func-names": 0,
-    "import/extensions": 0,
+  extends: ['airbnb-typescript', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  plugins: ['@typescript-eslint'],
+  ignorePatterns: ['__tests__'],
+  rules: {
+    'func-names': 0,
+    'import/extensions': 0,
     // "import/extensions": ["error", "always", {
     //     "js": "never",
     //     "jsx": "never",
     //     "ts": "never",
     //     "tsx": "never"
     // }],
-    "import/no-extraneous-dependencies": ["error", {
-      "devDependencies": true,
-      "optionalDependencies": false
-    }],
-    "import/prefer-default-export": "off",
-    "import/no-default-export": "error",
-    "no-alert": 0,
-    "no-console": 0,
-    "@typescript-eslint/explicit-function-return-type": 0,
-    "@typescript-eslint/interface-name-prefix": 0,
-    "@typescript-eslint/naming-convention": [
-      "error",
+    'import/no-extraneous-dependencies': [
+      'error',
       {
-        selector: "parameter",
-        format: ["camelCase"],
-        leadingUnderscore: "allow"
+        devDependencies: true,
+        optionalDependencies: false,
+      },
+    ],
+    'import/prefer-default-export': 'off',
+    'import/no-default-export': 'error',
+    'no-alert': 0,
+    'no-console': 0,
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/interface-name-prefix': 0,
+    '@typescript-eslint/naming-convention': [
+      'error',
+      {
+        selector: 'parameter',
+        format: ['camelCase'],
+        leadingUnderscore: 'allow',
       },
       {
-        selector: "class",
-        format: ["PascalCase"]
+        selector: 'class',
+        format: ['PascalCase'],
       },
       {
-        selector: "interface",
-        format: ["PascalCase"],
+        selector: 'interface',
+        format: ['PascalCase'],
         // custom: {
         //   regex: "^I[A-Z]",
         //   match: false
         // }
       },
       {
-        selector: "enum",
-        format: ["PascalCase"]
-      }
+        selector: 'enum',
+        format: ['PascalCase'],
+      },
     ],
-    "@typescript-eslint/no-explicit-any": 0,
-    "@typescript-eslint/prefer-nullish-coalescing": "error",
-    "@typescript-eslint/prefer-optional-chain": "error",
-    "prettier/prettier": "error"
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/prefer-nullish-coalescing': 'error',
+    '@typescript-eslint/prefer-optional-chain': 'error',
+    'prettier/prettier': 'error',
   },
-  "settings": {
-    "import/extensions": [".js", ".jsx", ".json", ".ts", ".tsx"],
+  settings: {
+    'import/extensions': ['.js', '.jsx', '.json', '.ts', '.tsx'],
     // "import/resolver": {
     //   "webpack": {
     //     "config": "webpack.config.js"
     //   }
     // }
-  }
+  },
 };
