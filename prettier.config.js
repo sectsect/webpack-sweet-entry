31e2de0f72fbefa95b50e70ac4cf23852912b241
sect <sect@sect.me>
2024-02-25 13:33:01 +0900

    chore: update dependencies

--- prettier.config.js
+++ prettier.config.js
@@ -1,4 +1,21 @@
 module.exports = {
-  ...require('@spotify/prettier-config'),
-  // your overrides here
+  // eslint-disable-next-line global-require, import/no-extraneous-dependencies
+  // ...require('@spotify/prettier-config'),
+  // printWidth: 80,
+  // tabWidth: 2,
+  // useTabs: false,
+  // semi: true,
+  singleQuote: true,
+  // jsxSingleQuote: false,
+  trailingComma: 'all',
+  // bracketSpacing: true,
+  // jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+  // rangeStart: 0,
+  // rangeEnd: Infinity,
+  // requirePragma: false,
+  // insertPragma: false,
+  // proseWrap: 'preserve',
+  // htmlWhitespaceSensitivity: 'css',
+  // endOfLine: 'auto',
 };
